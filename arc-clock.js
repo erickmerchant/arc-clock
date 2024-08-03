a020047fd5207c23e6f71e9d14b20e807ea2ee2f
erickmerchant <erickmerchant@gmail.com>
2024-08-03 01:17:58 -0400

    Inline css

--- arc-clock.js
+++ arc-clock.js
@@ -11,15 +11,75 @@ class ArcClock extends HTMLElement {
 
 		customElements.define("arc-clock", ArcClock);
 
-		this.#sheet.replaceSync("");
+		this.#sheet.replaceSync(`
+			@keyframes cycle {
+				0% {
+					--arc-clock--time: 0%;
+				}
 
-		let cssUrl = new URL("./arc-clock.css", import.meta.url);
+				100% {
+					--arc-clock--time: 100%;
+				}
+			}
 
-		fetch(cssUrl)
-			.then((res) => res.text())
-			.then((css) => {
-				this.#sheet.replaceSync(css);
-			});
+			:host {
+				grid-template-columns: var(--template);
+				grid-template-rows: var(--template);
+				gap: var(--arc-clock--gap);
+				--color: var(--arc-clock--hours-color);
+				--template: repeat(3, var(--arc-clock--size)) auto
+					repeat(3, var(--arc-clock--size));
+				--duration: var(--hours-duration);
+				--delay: var(--hours-delay);
+
+				&,
+				&::before,
+				&::after {
+					will-change: var(--arc-clock--time);
+					border-radius: 100%;
+					display: grid;
+					aspect-ratio: 1;
+					background: radial-gradient(
+							closest-side,
+							white,
+							white calc(100% - var(--arc-clock--size)),
+							oklab(0% 0 0 / 0) calc(100% - var(--arc-clock--size)),
+							oklab(0% 0 0 / 0)
+						),
+						conic-gradient(
+							var(--color),
+							var(--color) var(--arc-clock--time),
+							white var(--arc-clock--time)
+						);
+					animation-timing-function: linear;
+					animation-name: cycle;
+					animation-duration: var(--duration);
+					animation-delay: var(--delay);
+					animation-iteration-count: infinite;
+				}
+
+				&::before,
+				&::after {
+					content: "";
+					grid-column: var(--col-row);
+					grid-row: var(--col-row);
+				}
+
+				&::before {
+					--color: var(--arc-clock--minutes-color);
+					--col-row: 2 / -2;
+					--duration: 3600s;
+					--delay: var(--minutes-delay);
+				}
+
+				&::after {
+					--color: var(--arc-clock--seconds-color);
+					--col-row: 3 / -3;
+					--duration: 60s;
+					--delay: var(--seconds-delay);
+				}
+			}
+		`);
 	}
 
 	connectedCallback() {
@@ -34,15 +94,15 @@ class ArcClock extends HTMLElement {
 		let seconds = date.getSeconds();
 		let minutes = date.getMinutes() * 60 + seconds;
 		let hours = (date.getHours() % totalHours) * 3_600 + minutes;
-		let div = document.createElement("div");
+		let instance = new CSSStyleSheet();
 
-		div.style.setProperty("--hours-duration", `${totalHours * 3_600}s`);
-		div.style.setProperty("--hours-delay", `${-1 * hours}s`);
-		div.style.setProperty("--minutes-delay", `${-1 * minutes}s`);
-		div.style.setProperty("--seconds-delay", `${-1 * seconds}s`);
+		instance.replaceSync(`:host {
+			--hours-duration: ${totalHours * 3_600}s;
+			--hours-delay: ${-1 * hours}s;
+			--minutes-delay: ${-1 * minutes}s;
+			--seconds-delay: ${-1 * seconds}s;
+		}`);
 
-		shadow.adoptedStyleSheets = [ArcClock.#sheet];
-
-		shadow.append(div);
+		shadow.adoptedStyleSheets = [ArcClock.#sheet, instance];
 	}
 }
