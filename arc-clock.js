912bc860ee1b9641fb3c9b33b4a13fd7667ba215
erickmerchant <erickmerchant@gmail.com>
2024-08-31 23:40:41 -0400

    Changes

--- arc-clock.js
+++ arc-clock.js
@@ -1,5 +1,5 @@
 window.CSS.registerProperty({
-	name: "--time",
+	name: "--arc-clock--time",
 	syntax: "<percentage>",
 	inherits: true,
 	initialValue: "0%",
@@ -7,29 +7,25 @@ window.CSS.registerProperty({
 
 class ArcClock extends HTMLElement {
 	connectedCallback() {
-		let shadow = this.shadowRoot ?? this.attachShadow({mode: "open"});
+		let shadow = this.shadowRoot;
 		let totalHours = Number(this.getAttribute("total-hours"));
+		let div = document.createElement("div");
 
 		if (!totalHours || Number.isNaN(totalHours)) {
 			totalHours = 24;
 		}
 
+		shadow.append(div);
+
 		let date = new Date();
 		let seconds = date.getSeconds();
 		let minutes = date.getMinutes() * 60 + seconds;
 		let hours = (date.getHours() % totalHours) * 3_600 + minutes;
-		let instance = new CSSStyleSheet();
 
-		instance.replaceSync(`:host {
-			--hours-duration: ${totalHours * 3_600}s;
-			--hours-delay: ${-1 * hours}s;
-			--minutes-delay: ${-1 * minutes}s;
-			--seconds-delay: ${-1 * seconds}s;
-		}`);
-
-		shadow.adoptedStyleSheets = [instance];
-
-		shadow.append(document.createElement("div"));
+		div.style.setProperty("--hours-duration", `${totalHours * 3_600}s`);
+		div.style.setProperty("--hours-delay", `${-1 * hours}s`);
+		div.style.setProperty("--minutes-delay", `${-1 * minutes}s`);
+		div.style.setProperty("--seconds-delay", `${-1 * seconds}s`);
 	}
 }
 
